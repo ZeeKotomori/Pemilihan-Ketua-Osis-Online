befec6c6a393146a0bd4d438797863297017bdf2
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 13:55:54 +0700

    feat: add user management routes for create, read, update, and delete operations

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authenticateJwt.js";
-import { voteTeam } from "../controllers/userController.js";
+import { createUser, deleteUser, getUser, updateUser, voteTeam } from "../controllers/userController.js";
 import { checkUserRole } from "../middleware/roleUserCheckMiddleware.js";
 
 const router = express.Router();
 
 router.post("/vote-team", authMiddleware, checkUserRole(['u']), voteTeam);
+router.get("/get-user", authMiddleware, getUser);
+router.post("/create-user", authMiddleware, checkUserRole(['a']), createUser);
+router.patch("/update-user", authMiddleware, checkUserRole(['a']), updateUser);
+router.delete("/delete-user", authMiddleware, checkUserRole(['a']), deleteUser);
 
 export default router;
