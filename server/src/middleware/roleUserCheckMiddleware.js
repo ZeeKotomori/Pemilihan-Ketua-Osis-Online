a23650768c65d91a5a04feaa2c4ce31aba1d171b
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 09:09:40 +0700

    feat: add roleUserCheck middleware for user role validation in requests

---
+++ server/src/middleware/roleUserCheckMiddleware.js
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const checkUserRole = (allowedRoles) => {
+    return async (req, res, next) => {
+        const nisn = req.user.nisn;
+
+        if (!nisn) return res.status(401).send({ error: "Access denied. No token provided." });
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: { nisn },
+                select: { role: true },
+            });
+
+            if (!user) {
+                return res.status(404).send({ error: "User not found" });
+            }
+
+            if (!allowedRoles.includes(user.role)) {
+                return res.status(403).send({ error: "Access denied" });
+            }
+
+            next();
+        } catch (error) {
+            console.error(error);
+            return res.status(500).send({ error: "Internal Server Error" });
+        }
+    };
+}
