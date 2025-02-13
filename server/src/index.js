39f50acfeedd8fbf193f82423df7ce3ff0f45c14
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 08:54:28 +0700

    fix: update route paths for auth and data endpoints

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import { PrismaClient } from "@prisma/client";
 import authRoutes from "./routes/authRoute.js";
-import dataRoute from "./routes/dataRoute.js"
+import dataRoute from "./routes/casisRoute.js"
 
 dotenv.config();
 const app = express();
@@ -12,8 +12,8 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/auth", dataRoute);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1", dataRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
