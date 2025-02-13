d13e731bf45aed5a8d9f529a006992ec4e681f2a
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 13:55:17 +0700

    feat: enhance casisRoute with CRUD operations and file upload handling

--- server/src/routes/casisRoute.js
+++ server/src/routes/casisRoute.js
@@ -1,9 +1,30 @@
 import express from "express";
-import { getAllData, getDataByTeamName } from "../controllers/casisController.js";
+import { createCasis, deleteCasis, getAllData, getDataByTeamName, updateCasis } from "../controllers/casisController.js";
+import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get("/", getAllData);
-router.get("/team", getDataByTeamName);
+router.get("/team/:teamName", getDataByTeamName);
+
+router.post(
+    "/add-casis",
+    upload.fields([
+        { name: "leaderPhoto", maxCount: 1 },
+        { name: "coLeaderPhoto", maxCount: 1 },
+    ]),
+    createCasis
+);
+
+router.patch(
+    "/update-casis",
+    upload.fields([
+        { name: "leaderPhoto", maxCount: 1 },
+        { name: "coLeaderPhoto", maxCount: 1 },
+    ]),
+    updateCasis
+);
+
+router.delete("/delete", deleteCasis)
 
 export default router;
