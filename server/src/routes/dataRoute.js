cf211e21e90cf171e184f07434e98d3a8503d6dd
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 08:51:42 +0700

    rename to casisController and casisRoute

--- server/src/routes/dataRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllData, getDataByTeamName } from "../controllers/dataController.js";
-
-const router = express.Router();
-
-router.get("/", getAllData);
-router.get("/team", getDataByTeamName);
-
-export default router;
