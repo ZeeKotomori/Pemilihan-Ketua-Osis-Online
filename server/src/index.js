4c8b30905b728bcd95fa915abd1a6399c860883c
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 13:54:30 +0700

    feat: update server index to serve static files and improve error handling

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
 import authRoutes from "./routes/authRoute.js";
-import dataRoute from "./routes/casisRoute.js"
+import casisRoute from "./routes/casisRoute.js"
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
-const prisma = new PrismaClient();
+
+app.use('/public', express.static(path.resolve(__dirname, '../public')));
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1", dataRoute);
+app.use("/api/v1", casisRoute);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ message: 'Terjadi kesalahan pada server' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
