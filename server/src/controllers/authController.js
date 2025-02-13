36e5da7319cb7bb86188619f12d35000f14e1c08
ZeeKotomori <zikriaf23@gmail.com>
2025-02-13 08:55:01 +0700

    feat: refactor authController for registration and login functionality

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,10 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-export const requestOTP = async (req, res) => {
+export const register = async (req, res) => {
     const { nisn, email, fullName } = req.body;
     if (!nisn || !email || !fullName) return res.status(404).send("please fill the form");
 
@@ -17,10 +18,10 @@ export const requestOTP = async (req, res) => {
         return res.status(400).send("You already using this email or NISN. If you don't recognize this, please contact the committee.");
     }
 
-    const otp = crypto.randomInt(100000, 999999).toString();
+    const email = crypto.randomInt(100000, 999999).toString();
 
     await prisma.student.create({
-        data: { nisn, email, fullName, otp },
+        data: { nisn, email, fullName, email },
     });
 
     const transporter = nodemailer.createTransport({
@@ -34,43 +35,38 @@ export const requestOTP = async (req, res) => {
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
         to: email,
-        subject: "Your OTP Code",
-        text: `Your OTP code for voting is: ${otp}`,
+        subject: "email CODE FOR VOTING",
+        text: `Your email code for voting is: ${email}, make sure you don't give it to everyone, if I don't ask for this email, contact the committee`,
     });
 
-        return res.status(200).send({ msg: "OTP sent to email." });
+        return res.status(200).send({ msg: "email sent to email." });
     } catch (error) {
         console.log(error);
         return res.status(500).send("Internal Server Error");
     }
 };
 
-export const verifyOTP = async (req, res) => {
-    const { email, otp, teamName } = req.body;
-
-    if (!email || !otp || !teamName) return res.status(400).send("please input email, otp, and team name");
+export const login = async (req, res) => {
+    const { nisn, email } = req.body;
+    if (!nisn || !email) return res.status(404).send("please fill the form");
 
     try {
-        const student = await prisma.student.findUnique({ where: { email } });
+        const user = await prisma.student.findFirst({
+            where: { nisn, email }
+        });
 
-        if (!student || student.otp !== otp) {
-            return res.status(400).send({ msg: "Invalid OTP." });
+        if (!user) {
+            return res.status(400).send("Invalid NISN or email.");
         }
 
-        if (student.isVoted === true) {
-            return res.status(400).send({ msg: "OTP has been used. If you feel you have not voted, please inform the committee." });
-        }
+        const token = jwt.sign({ id: user.email, nisn : user.nisn}, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-        await prisma.student.update({
-            where : { email },
-            data : {
-                isVoted : true,
-            },
-        })
-
-        return res.status(200).send({ msg: "OTP verified successfully." });
+        return res.status(200).send({ msg: "Login successful.", token });
     } catch (error) {
         console.log(error);
         return res.status(500).send("Internal Server Error");
     }
-};
+}
+export const logout = (req, res) => {
+    return res.status(200).send({ msg: "Logout successful." });
+}
