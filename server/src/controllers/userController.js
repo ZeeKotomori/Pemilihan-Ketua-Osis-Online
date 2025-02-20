0a356e595274934ab5146a777be8a7901f980278
ZeeKotomori <zikriaf23@gmail.com>
2025-02-20 08:11:09 +0700

    feat: refactor userController to replace Student model with User model and update related logic

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,44 +2,44 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
-export const voteTeam = async (req, res) => {
-    const { email, otp, teamName } = req.body;
+// export const voteTeam = async (req, res) => {
+//     const { email, otp, teamName } = req.body;
 
-    if (!email || !otp || !teamName) return res.status(400).send("please input email, otp, and team name");
+//     if (!email || !otp || !teamName) return res.status(400).send("please input email, otp, and team name");
 
-    try {
-        const student = await prisma.student.findUnique({ where: { email } });
+//     try {
+//         const user = await prisma.user.findUnique({ where: { email } });
 
-        if (!student || student.otp !== otp) {
-            return res.status(400).send({ msg: "Invalid OTP." });
-        }
+//         if (!user || user.otp !== otp) {
+//             return res.status(400).send({ msg: "Invalid OTP." });
+//         }
 
-        if (student.isVoted === true) {
-            return res.status(400).send({ msg: "OTP has been used. If you feel you have not voted, please inform the committee." });
-        }
+//         if (user.isVoted === true) {
+//             return res.status(400).send({ msg: "OTP has been used. If you feel you have not voted, please inform the committee." });
+//         }
 
-        await prisma.student.update({
-            where : { email },
-            data : {
-                isVoted : true,
-            },
-        })
+//         await prisma.user.update({
+//             where : { email },
+//             data : {
+//                 isVoted : true,
+//             },
+//         })
 
-        return res.status(200).send({ msg: "OTP verified successfully." });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send("Internal Server Error");
-    }
-};
+//         return res.status(200).send({ msg: "OTP verified successfully." });
+//     } catch (error) {
+//         console.log(error);
+//         return res.status(500).send("Internal Server Error");
+//     }
+// };
 
 export const createUser = async (req, res) => {
-    const { email, otp } = req.body;
+    const { email, password, fullName, kelas } = req.body;
 
-    if (!email || !otp) return res.status(400).send("Please provide email and otp");
+    if (!email || !password || !fullName || !kelas) return res.status(400).send("Please provide email and otp");
 
     try {
-        const newUser = await prisma.student.create({
-            data: { email, otp, isVoted: false },
+        const newUser = await prisma.user.create({
+            data: { email, password, fullName, kelas },
         });
         return res.status(201).send(newUser);
     } catch (error) {
@@ -52,7 +52,7 @@ export const getUser = async (req, res) => {
     const { email } = req.params;
 
     try {
-        const user = await prisma.student.findUnique({ where: { email } });
+        const user = await prisma.user.findUnique({ where: { email } });
 
         if (!user) return res.status(404).send("User not found");
 
@@ -64,13 +64,18 @@ export const getUser = async (req, res) => {
 };
 
 export const updateUser = async (req, res) => {
-    const { email } = req.params;
-    const { otp, isVoted } = req.body;
+    const { id } = req.params;
+    const { email, fullName, password, kelas } = req.body;
 
     try {
-        const updatedUser = await prisma.student.update({
-            where: { email },
-            data: { otp, isVoted },
+        const updatedUser = await prisma.user.update({
+            where: { id : id },
+            data: {
+                email : email,
+                fullName : fullName,
+                password : password,
+                kelas : kelas
+            }
         });
 
         return res.status(200).send(updatedUser);
@@ -81,10 +86,10 @@ export const updateUser = async (req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
-    const { email } = req.params;
+    const { id } = req.params;
 
     try {
-        await prisma.student.delete({ where: { email } });
+        await prisma.user.delete({ where: { id } });
         return res.status(200).send("User deleted successfully");
     } catch (error) {
         console.log(error);
